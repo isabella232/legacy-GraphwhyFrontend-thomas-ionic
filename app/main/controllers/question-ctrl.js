f1ea8e83452775e752282c4f8496e132c3a0d8da
ammc <alexander.hdwctw@gmail.com>
2016-01-14 11:20:52 -0800

    add more questions

--- app/main/controllers/question-ctrl.js
+++ app/main/controllers/question-ctrl.js
@@ -9,27 +9,43 @@ angular.module('main')
   var questions = [
     {
       title: 'Are you concerned about the US national debt?',
-      options: ['Yes, absolutely', 'Yes', 'No', 'No, not at all', 'none of the above']
+      options: ['Yes, absolutely', 'Yes, mostly', 'yes, slightly', 'No', 'none of the above']
+    },
+    {
+      title: 'Would you spend an hour a month reviewing government policies?',
+      options: ['Yes, absolutely', 'Yes, mostly', 'yes, slightly', 'No', 'none of the above']
     },
     {
       title: 'Have you learned about US national debt in a classroom?',
-      options: ['yes, absolutely', 'yes', 'no', 'No, not at all', 'none of the above']
+      options: ['yes, absolutely', 'yes, mostly', 'yes, slightly', 'No', 'none of the above']
     },
     {
       title: 'Do you trust federal government policy makers?',
-      options: ['yes, absolutely', 'yes', 'no', 'No, not at all', 'none of the above']
-    },
-    {
-      title: 'Do you support legalizing and taxing all recreational drugs?',
-      options: ['yes, absolutely', 'yes', 'no', 'No, not at all', 'none of the above']
+      options: ['yes, absolutely', 'yes, mostly', 'yes, slightly', 'No', 'none of the above']
     },
     {
       title: 'Do you know approximate amount of US national debt?',
-      options: ['yes, absolutely', 'yes', 'no', 'No, not at all', 'none of the above']
+      options: ['yes, absolutely', 'yes, mostly', 'yes, slightly', 'No', 'none of the above']
     },
     {
       title: 'Do you know why the US is in debt?',
-      options: ['yes, absolutely', 'yes', 'no', 'No, not at all', 'none of the above']
+      options: ['yes, absolutely', 'yes, mostly', 'yes, slightly', 'No', 'none of the above']
+    },
+    {
+      title: 'Do you support legalizing and taxing of recreational drugs?',
+      options: ['yes, absolutely', 'yes, mostly', 'yes, slightly', 'No', 'none of the above']
+    },
+    {
+      title: 'Do you support a lower miliary budget?',
+      options: ['yes, absolutely', 'yes, mostly', 'yes, slightly', 'No', 'none of the above']
+    },
+    {
+      title: 'Do you support stricter tax laws for US corporations',
+      options: ['yes, absolutely', 'yes, mostly', 'yes, slightly', 'No', 'none of the above']
+    },
+    {
+      title: 'Do you support internation free trade agreements',
+      options: ['yes, absolutely', 'yes, mostly', 'yes, slightly', 'No', 'none of the above']
     }
   ];
   $scope.counter = parseInt($stateParams.qnum)+1;
